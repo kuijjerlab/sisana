68de84b49d8a3bb93da8cfbcbc3be77f91e41221
newmanno <newmanno@oregonstate.edu>
2025-08-04 11:19:35 +0200

    Fixed text overlapping issue in genelist genes

--- sisana/analyze_networks/volcano_plot.py
+++ sisana/analyze_networks/volcano_plot.py
@@ -22,14 +22,16 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
         - diffcol: str, The name of the column containing the difference in medians or means
         - adjpcol: str, The name of the column containing the adj. p-value
         - adjpvalthreshold: str, Threshold to use for the adjusted p-value
-        - genelist: str, Path to a .txt file containing a list of genes to plot. Alternatively, the top {numlabels} genes can be plotted instead if top=True.
-        - outdir: str, Path to directory to output file to
+        - xaxisthreshold: int, Threshold to use for the x-axis (value will also be applied to the negative end of the x axis, 
+          meaning a value of 50 really means +/- 50)
         - difftype: str, The type of difference to use for the x-axis. "mean" will be difference in means and "median"
           refers to difference in medians
+        - outdir: str, Path to directory to output file to
         - top: Flag for whether to automatically label the top 10 values. Does not use the genelist in this case, but rather finds the top genes
           based on FDR and fold change.
         - numlabels: int, Number of top values to label. Can only be used if top=True.
-        
+        - genelist: str, Path to a .txt file containing a list of genes to plot. Alternatively, the top {numlabels} genes can be plotted instead if top=True.
+
     Returns:
     -----------
         - string of the output file path 
@@ -46,7 +48,9 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
     plt.scatter(x=stats[diffcol],y=stats[adjpcol].apply(lambda x:-np.log10(x)), s=1)
 
     down = stats[(stats[diffcol] <= xaxisthreshold) & (stats[adjpcol] <= adjpvalthreshold)]
+    alldown = stats[stats[diffcol] < 0] # df of all genes that are down-regulated (not necessarily significant)
     up = stats[(stats[diffcol] > xaxisthreshold) & (stats[adjpcol] <= adjpvalthreshold)]
+    allup = stats[stats[diffcol] > 0] # df of all genes that are up-regulated (not necessarily significant)
     not_down_or_up = stats[(stats[diffcol] < xaxisthreshold) & (stats[diffcol] > -1 * xaxisthreshold) & (stats[adjpcol] < adjpvalthreshold)]
     notsig = stats[stats[adjpcol] > adjpvalthreshold]
     
@@ -57,27 +61,28 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
 
     plt.scatter(x=down[diffcol], y=down[adjpcol].apply(lambda x:-np.log10(x)), s=3, label=f"Up in {group_names[1]}", color="blue")
     plt.scatter(x=up[diffcol], y=up[adjpcol].apply(lambda x:-np.log10(x)), s=3, label=f"Up in {group_names[0]}", color="orange")
-    plt.scatter(x=notsig[diffcol], y=notsig[adjpcol].apply(lambda x:-np.log10(x)), s=3, label="Not significant", color="gainsboro")
-    plt.scatter(x=not_down_or_up[diffcol], y=not_down_or_up[adjpcol].apply(lambda x:-np.log10(x)), s=3, color="darkgrey")
+    plt.scatter(x=notsig[diffcol], y=notsig[adjpcol].apply(lambda x:-np.log10(x)), s=3, color="gainsboro")
+    plt.scatter(x=not_down_or_up[diffcol], y=not_down_or_up[adjpcol].apply(lambda x:-np.log10(x)), s=3, color="gainsboro")
 
     # Label the user-defined genes if given, otherwise plot just the top genes
     if genelist != None:
         # Find the overlap in the user gene list and the up/down genes 
         genelist = file_to_list(genelist)
         
-        user_defined_upgenes = list(set(genelist).intersection(up.index))
-        user_defined_dngenes = list(set(genelist).intersection(down.index))
+        user_defined_upgenes = list(set(genelist).intersection(allup.index))
+        user_defined_dngenes = list(set(genelist).intersection(alldown.index))
         
-        if len(user_defined_upgenes) + len(user_defined_dngenes) != len(genelist):
-            warnings.warn("\nWarning: Some genes in the input list are not significant and will not be labeled.")
+        # if len(user_defined_upgenes) + len(user_defined_dngenes) != len(genelist):
+        #     warnings.warn("\nWarning: Some genes in the input list are not significant and will not be labeled.")
 
-        up_subset = filter_for_user_defined_genes(datafile=up, genes=user_defined_upgenes, verbose=False)
-        dn_subset = filter_for_user_defined_genes(datafile=down, genes=user_defined_dngenes, verbose=False)
+        # Label all user-defined genes, regardless if they are significant or not
+        up_subset = filter_for_user_defined_genes(datafile=allup, genes=user_defined_upgenes, verbose=False)
+        dn_subset = filter_for_user_defined_genes(datafile=alldown, genes=user_defined_dngenes, verbose=False)
         
         uptexts = create_label_list(statsfile=up_subset, difference_column=diffcol, adjp_column=adjpcol)
         dntexts = create_label_list(statsfile=dn_subset, difference_column=diffcol, adjp_column=adjpcol)
 
-    else: # Plot the top genes
+    else: # Label just the top genes
         print("No list of genes to label was given, so labeling the top genes instead. Adjust the 'numlabels' parameter to change the number of genes labeled.")
         if len(up) >= numlabels:
             up_subset = up.sort_values(adjpcol, ascending=True).head(numlabels)
@@ -96,8 +101,14 @@ def plot_volcano(statsfile: str, diffcol: str, adjpcol: str, adjpvalthreshold: s
             
         dntexts = create_label_list(statsfile=dn_subset, difference_column=diffcol, adjp_column=adjpcol)
 
+    # Add more space at top of plot for labeling
+    ax = plt.gca()
+    ymin, ymax = ax.get_ylim()
+    ymax = ymax*1.05
+    ax.set_ylim([ymin, ymax])
+
     # Add labels to plot
-    adjust_text(uptexts,arrowprops=dict(arrowstyle="-", color='black', lw=0.5))        
+    adjust_text(uptexts,arrowprops=dict(arrowstyle="-", color='black', lw=0.5), force_text=(2,2))        
     adjust_text(dntexts,arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
     
     if difftype == "mean":
