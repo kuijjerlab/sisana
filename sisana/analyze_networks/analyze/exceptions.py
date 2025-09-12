2626f1133391be892174b4661392ef9839a21b75
newmanno <newmanno@oregonstate.edu>
2025-09-12 10:54:37 +0200

    minor changes

--- sisana/analyze_networks/analyze/exceptions.py
+++ sisana/analyze_networks/analyze/exceptions.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import numpy as np
+import sys 
 
 class NotASubsetError(Exception):
     """
     Raise when the supplied list (genes/samples) is not a subset of the items in the data frame
     
     Attributes:
-        user_gene_list : List of genes/samples the user inputs
-        data_genes : List of genes/samples in the dataset
+        user_list : List of genes/samples the user inputs
+        data_list : List of genes/samples in the dataset
         dtype : data type that makes up the list, either "genes" or "samples" 
         message -- explanation of the error
     """
 
-    def __init__(self, user_list, data_list, dtype, message="Error: The genes in the supplied list are not a subset of the genes in the dataset."):
+    def __init__(self, user_list, data_list, dtype, message="Error: The items in the supplied list are not a subset of the data."):
         
         self.user_list = user_list
         self.data_list = np.unique(data_list)
         self.dtype = dtype
-        
-        items_missing = list(set(self.user_list).difference(self.data_list)) # Find genes/samples not in the data provided
-        print(items_missing)
+                
+        # Not sure if order matters here. Keeping it for testing purposes later, but it at least does catch the error for now
+        # items_missing = list(set(self.user_list).difference(self.data_list)) # Find genes/samples not in the data provided
+        items_missing = list(set(self.data_list).difference(self.user_list)) # Find genes/samples not in the data provided
         print(f"\nError: The following {self.dtype} in the supplied list are not present in the data provided:")
         [print(i) for i in items_missing]
         print(f"\nYou have provided the following {self.dtype}:")
