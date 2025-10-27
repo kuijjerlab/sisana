4d76538816e4594b9c679a792c8f9372ed180d76
newmanno <newmanno@oregonstate.edu>
2025-10-27 10:18:35 +0100

    Added hotstart option

--- sisana/postprocessing/lioness_to_pickle_df.py
+++ sisana/postprocessing/lioness_to_pickle_df.py
@@ -2,27 +2,27 @@
 import numpy as np
 import pandas as pd
 import argparse
-from .post import files_to_dfs
 import sys
 
-def convert_lion_to_pickle(panda: str, lion: str, type: str, names: str, outfile: str, start: int=None, end: int=None):    
+def convert_lion_to_pickle(panda: str, lion: str, type: str, names: str, outfile: str, start: int=None, end: int=None, hotstart: bool=False):    
     '''
     Creates data frames from the input panda and lioness files        
      
     Parameters:
     -----------
         - panda: str, Path to panda output file
-        - lion: str, lioness data frame
+        - lion: pd.DataFrame, lioness data frame
         - type: str, file type of lioness file, either npy or txt
         - names: str, File with list of sample names (one per line) in the same order that were supplied for panda/lioness
         - outfile: str, Path to output file in pickle format (e.g. lioness.pickle)
-        - start: the starting index of samples that networks were created for
-        - end: the ending index of samples that networks were created for
+        - start: the starting index of samples that networks were created for. Only specify this value if "start" and "stop" params were used in the params.yml file.
+        - end: the ending index of samples that networks were created for. Only specify this value if "start" and "stop" params were used in the params.yml file.
         
     Returns:
     -----------
         - Nothing
     '''
+    
     # Create data frames from input files
     pan = pd.read_csv(panda, sep = " ", engine = "python")
     pan["TF-gene"] = "TF_" + pan["tf"] + "<==>" + pan["gene"]
@@ -32,7 +32,7 @@ def convert_lion_to_pickle(panda: str, lion: str, type: str, names: str, outfile
     with open(names, 'r') as file:
         for line in file: 
             samps.append(line.strip())
-
+            
     lion.index = pan["TF-gene"] 
 
     if start is not None:
@@ -42,5 +42,25 @@ def convert_lion_to_pickle(panda: str, lion: str, type: str, names: str, outfile
     
     lion.to_pickle(outfile)   
  
+# For hot-starting in case of crashing, so the user does not need to reconstruct all the networks again
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()    
+    parser.add_argument('-p', '--panda', help='Path to panda output file')    
+    parser.add_argument('-l', '--lion', help='lioness data frame')    
+    parser.add_argument('-t', '--type', help='file type of lioness file, either npy or txt')
+    parser.add_argument('-n', '--names', help='File with list of sample names (one per line) in the same order that were supplied for panda/lioness')
+    parser.add_argument('-o', '--outfile', help='Path to output file in pickle format (e.g. lioness.pickle)')
+    parser.add_argument('-s', '--start', default=None, help='OPTIONAL, the starting index of samples that networks were created for. Only specify this value if "start" and "stop" params were used in the params.yml file.')
+    parser.add_argument('-e', '--end', default=None, help='Prints the version of SiSaNA currently being used.')
+    args = parser.parse_args()
     
+    lioness_array = np.load(args.lion)
+    lioness_df = pd.DataFrame(lioness_array)
     
+    convert_lion_to_pickle(args.panda, 
+                           lioness_df,
+                           args.type,
+                           args.names,
+                           args.outfile,
+                           args.start,
+                           args.end)    
